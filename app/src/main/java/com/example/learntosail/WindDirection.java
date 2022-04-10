6fadc60abb92a4dc91650bc83160867e7d9e62b9
acmercer <acmercerr@gmail.com>
2022-04-10 13:34:22 -0700

    Updates and changes

--- app/src/main/java/com/example/learntosail/WindDirection.java
+++ app/src/main/java/com/example/learntosail/WindDirection.java
@@ -1,5 +1,25 @@
 package com.example.learntosail;
 
-public class WindDirection {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class WindDirection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.winddirection);
+
+        //Setting up different image views
+        ImageButton imgOp1, imgOp2, imgOp3, imgOp4;
+        imgOp1 = findViewById(R.id.btnOption1);
+        imgOp2 = findViewById(R.id.btnOption2);
+        imgOp3 = findViewById(R.id.btnOption3);
+        imgOp4 = findViewById(R.id.btnOption4);
+    }
+    public void question1() {
+
+    }
 }
